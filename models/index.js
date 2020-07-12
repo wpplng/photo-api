c20849630d108e4fd4122ccc7579b0f547684e16
Lina Väppling <lina.wappling@gmail.com>
2020-07-12 13:57:14 +0200

    More improvements + added env.example and set up database connection

---
+++ models/index.js
@@ -0,0 +1,20 @@
+/**
+ * Setting up the database connection
+ */
+
+const knex = require('knex')({
+	client: 'mysql',
+	connection: {
+		host: process.env.DB_HOST || 'localhost',
+		port: process.env.DB_PORT || 3306,
+		user: process.env.DB_USER || 'library',
+		password: process.env.DB_PASSWORD || '',
+		database: process.env.DB_NAME || 'library',
+	},
+});
+
+const bookshelf = require('bookshelf')(knex);
+
+module.exports = {
+	bookshelf,
+};
