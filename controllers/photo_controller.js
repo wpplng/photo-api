bb75b9a73bbef4783e75ec3ceae10e8bb8cd221a
Lina Väppling <lina.wappling@gmail.com>
2020-07-14 12:23:55 +0200

    Show all photos and photos by id

--- controllers/photo_controller.js
+++ controllers/photo_controller.js
@@ -2,13 +2,66 @@
  * Photo Controller
  */
 
-const models = require('../models');
+const { matchedData, validationResult } = require('express-validator');
+const { Photo, User } = require('../models');
 
-// Get index (GET /)
-const index = async (req, res) => {};
+/* Get all photos for a user */
+const index = async (req, res) => {
+	// query db for user and photos relation
+	let user = null;
+	try {
+		user = await User.fetchById(req.user.data.id, {
+			withRelated: 'photos',
+		});
+	} catch (error) {
+		res.status(404).send({
+			status: 'fail',
+			data: 'Not found.',
+		});
+		return;
+	}
+
+	// get the photos that this user has
+	const photos = user.related('photos');
+
+	res.send({
+		status: 'success',
+		data: {
+			photos,
+		},
+	});
+};
 
 // Get a specific id (GET /id)
-const show = async (req, res) => {};
+const show = async (req, res) => {
+	let photo = null;
+	try {
+		photo = await Photo.fetchById(req.params.photoId);
+	} catch (error) {
+		res.status(404).send({
+			status: 'fail',
+			data: 'Photo not found.',
+		});
+		return;
+	}
+
+	const userId = photo.get('user_id');
+
+	if (userId !== req.user.data.id) {
+		res.status(401).send({
+			status: 'fail',
+			data: `You don't have access to that photo.`,
+		});
+		return;
+	}
+
+	res.send({
+		status: 'success',
+		data: {
+			photo,
+		},
+	});
+};
 
 // Create (POST /)
 const store = async (req, res) => {};
--- models/Photo.js
+++ models/Photo.js
@@ -3,13 +3,21 @@
  */
 
 module.exports = (bookshelf) => {
-	return bookshelf.model('Photo', {
-		tableName: 'photos',
-		albums() {
-			return this.belongsToMany('Album');
+	return bookshelf.model(
+		'Photo',
+		{
+			tableName: 'photos',
+			albums() {
+				return this.belongsToMany('Album');
+			},
+			user() {
+				return this.belongsTo('User');
+			},
 		},
-		user() {
-			return this.belongsTo('User');
-		},
-	});
+		{
+			fetchById(id, fetchOptions = {}) {
+				return new this({ id }).fetch(fetchOptions);
+			},
+		}
+	);
 };
